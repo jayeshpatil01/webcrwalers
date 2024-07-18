0da34dd9615823f1b32abf84cf1a8280fd47c0fd
Jay <jayeshpatil8041@gmail.com>
2024-07-18 15:56:49 +0530

    fetch items per page count

--- scrape_hockey_teams.rb
+++ scrape_hockey_teams.rb
@@ -1,5 +1,6 @@
 require 'selenium-webdriver'
 require 'nokogiri'
+require 'pry'
 
 url = 'https://www.scrapethissite.com/pages/forms/'
 options = Selenium::WebDriver::Chrome::Options.new
@@ -13,8 +14,11 @@ def hockey_teams(page_source, base_url)
   html = Nokogiri::HTML(page_source)
   teams = html.css('.team')
   next_page_link = html.css('.pagination li a').last['href']
+  per_page_option_selected = html.css('#per_page option[selected]')
+  per_page_option_selected = html.css('#per_page option').first if per_page_option_selected.empty?
+  per_page_option_selected = per_page_option_selected.text
   parsed_url = URI.parse(base_url)
-  [parsed_url.scheme, parsed_url.host]
+  [per_page_option_selected, parsed_url.scheme, parsed_url.host]
 end
 
 puts hockey_teams(page_source, url)
